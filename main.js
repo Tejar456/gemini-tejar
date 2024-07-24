bc0eed2a56f212c21b7a45ef99af04f37803ff87
tejarm04 <tejarm04@gmail.com>
2024-07-24 07:01:27 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import Base64 from 'base64-js';
+import MarkdownIt from 'markdown-it';
+import { maybeShowApiKeyBanner } from './gemini-api-banner';
+import './style.css';
+
+let API_KEY = 'AIzaSyBK4UCTTvpoFKkCilkrZVx6w6xho2HvOGY';
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Generating...';
+
+  try {
+   
+    let contents = [
+      {
+        role: 'user',
+        parts: [
+          { text: promptInput.value }
+        ]
+      }
+    ];
+
+  
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+      ],
+    });
+
+    const result = await model.generateContentStream({ contents });
+
+   
+    let buffer = [];
+    let md = new MarkdownIt();
+    for await (let response of result.stream) {
+      buffer.push(response.text());
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
+
+maybeShowApiKeyBanner(API_KEY);
